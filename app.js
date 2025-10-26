4fb985cb9a269f913b7f3d5a512fbdcfb56e99f1
Marco <martinezramirezm82@gmail.com>
2025-10-26 14:49:57 -0600

    por el momento se agrego configuracion de imagenes en el app.js

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json()); 
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.use('/propiedades', propiedadRoutes);
 app.use('/rentero', renteroRoutes);
